8d321eb7071595c3fe4f0704ec354c90350abd9c
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-25 16:50:45 -0500

    How many completed? v2

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,8 +8,9 @@ request.get(url, (err, response, body) => {
     console.error(err);
   }
   const completedTasks = {};
+  const tasks = JSON.parse(body);
 
-  for (const task of JSON.parse(body)) {
+  for (const task of tasks) {
     if (task.completed) {
       if (!completedTasks[task.userId]) {
         completedTasks[task.userId] = 1;
