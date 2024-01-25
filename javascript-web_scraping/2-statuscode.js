108d3835e393688a6c28e3bf5397ad3241990e08
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-24 19:03:10 -0500

    Status code v0

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const path = process.argv[2];
+
+request(path, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
