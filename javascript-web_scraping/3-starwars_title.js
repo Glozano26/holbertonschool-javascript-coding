4140dd3adb116d63bd0d7b9941cb2e0b3a25c66e
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-24 20:38:46 -0500

    Star wars movie title v0

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+const ids = process.argv[2];
+
+request.get(`${apiUrl}${ids}`, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
