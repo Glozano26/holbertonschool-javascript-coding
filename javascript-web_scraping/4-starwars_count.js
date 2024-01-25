e4ec9fd24b6714cf4ba25347692c617edf724d82
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-25 12:01:17 -0500

    Star wars Wedge Antilles v0

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  let len = 0;
+  for (const movie of JSON.parse(body).results) {
+    for (const character of movie.characters) {
+      if (character.includes(18)) {
+        len++;
+      }
+    }
+  }
+  console.log(len);
+});
