29e3d9225e139ae1fb90d0252999cc3b06785a89
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-25 12:40:28 -0500

    update show format JSon and know struct

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -9,5 +9,8 @@ request.get(`${apiUrl}${ids}`, (error, response, body) => {
     console.error(error);
   } else {
     console.log(JSON.parse(body).title);
+    const data = JSON.parse(body);
+    // console.log(JSON.stringify(data, null, 2));
+    // console.log(data);
   }
 });
