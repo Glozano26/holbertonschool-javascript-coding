5e4f54c424547d99b589d646f20d8974bcc0160f
glozano26 <lozsuperbia2014@gmail.com>
2024-02-08 18:32:33 -0500

    Reading a file synchronously with Node JS v1

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -15,9 +15,9 @@ function countStudents(pathData) {
     const CSStudents = students.filter((student) => student.field === 'CS');
     const SWEStudents = students.filter((student) => student.field === 'SWE');
 
-    console.log('Number of students:', students.length);
-    console.log('Number of students in CS:', CSStudents.length, 'List:', CSStudents.map((student) => student.firstname).join(', '));
-    console.log('Number of students in SWE:', SWEStudents.length, 'List:', SWEStudents.map((student) => student.firstname).join(', '));
+    console.log('Number of students:', String(students.length));
+    console.log('Number of students in CS:', String(CSStudents.length), 'List:', CSStudents.map((student) => student.firstname).join(', '));
+    console.log('Number of students in SWE:', String(SWEStudents.length), 'List:', SWEStudents.map((student) => student.firstname).join(', '));
   } catch (error) {
     throw new Error('Cannot load the database: ', `${error}`);
   }
