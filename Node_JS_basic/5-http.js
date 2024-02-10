cca46adf7e90d32426677da606b49fe5f3e95a9d
glozano26 <lozsuperbia2014@gmail.com>
2024-02-10 11:46:42 -0500

    Create a small HTTP server using Express v0

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,24 +1,53 @@
 const http = require('http');
-
-const countStudents = require('./3-read_file_async.js');
+const parseCSV = require('csvtojson'); // Use external library for structured parsing
 
 const port = 1245;
+const csvFile = process.argv[2]; // Access database name from argument
+
+function countStudents(csvPath) {
+  return parseCSV() // Use promises for cleaner async handling
+    .fromFile(csvPath)
+    .then((data) => {
+      // Filter out empty lines and invalid students
+      const students = data.filter((student) => student.length > 0 && student.hasOwnProperty('First Name'));
+
+      const totalStudents = students.length;
+      const CSStudents = students.filter((student) => student['Program'] === 'CS');
+      const SWEStudents = students.filter((student) => student['Program'] === 'SWE');
+
+      return { totalStudents, CSStudents, SWEStudents };
+    });
+}
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    countStudents(csvFile)
+      .then((students) => {
+        const { totalStudents, CSStudents, SWEStudents } = students;
+
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello Holberton School!\n');
-    } else if (req.url === '/students') {
-        readFile(process.argv[2], 'utf8', (err, data) => {
-            
-            const students = countStudents(data);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(`This is the list of our students\n${students}`);
-        });
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${totalStudents}\n`);
+        res.write(`Number of students in CS: ${CSStudents.length}. List: ${CSStudents.map((student) => student['First Name']).join(', ')}\n`);
+        res.write(`Number of students in SWE: ${SWEStudents.length}. List: ${SWEStudents.map((student) => student['First Name']).join(', ')}\n`);
+        res.end();
+      })
+      .catch((error) => {
+        console.error(error);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error\n');
+      });
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Page not found\n');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// eslint-disable-next-line
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const server = app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = server;
